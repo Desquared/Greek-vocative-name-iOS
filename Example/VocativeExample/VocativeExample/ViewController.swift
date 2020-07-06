ed69078f5c365ba7e539aea0b42c8a59b81b6a6d
Nikiforos Manalis <manalis@thinkdesquared.com>
2020-07-06 14:29:18 +0300

    Init vocative example

---
+++ Example/VocativeExample/VocativeExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VocativeExample
+//
+//  Created by Nikiforos Manalis on 6/7/20.
+//  Copyright © 2020 Desquared. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
