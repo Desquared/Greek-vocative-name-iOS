ae203825b43163e537913fb74539dc2e69c87143
Nikiforos Manalis <manalis@thinkdesquared.com>
2020-07-06 16:18:36 +0300

    Added test cases

--- Example/VocativeExample/VocativeExampleTests/VocativeExampleTests.swift
+++ Example/VocativeExample/VocativeExampleTests/VocativeExampleTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import VocativeNames
 @testable import VocativeExample
 
 class VocativeExampleTests: XCTestCase {
@@ -22,6 +23,112 @@ class VocativeExampleTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        var testName: String? = "Νικηφόρος"
+               XCTAssertEqual("Νικηφόρε", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Μάκης"
+               XCTAssertEqual("Μάκη", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Κοσμάς"
+               XCTAssertEqual("Κοσμά", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Κώστας"
+               XCTAssertEqual("Κώστα", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Θοδωρής"
+               XCTAssertEqual("Θοδωρή", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Γιάννης"
+               XCTAssertEqual( "Γιάννη", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Πέτρος"
+               XCTAssertEqual("Πέτρο", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Νίκος"
+               XCTAssertEqual("Νίκο", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Στυλιανός"
+               XCTAssertEqual("Στυλιανέ", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Στέλιος"
+               XCTAssertEqual("Στέλιο", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Ευαγγελάτος"
+               XCTAssertEqual("Ευαγγελάτο", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Ιωσήφ"
+               XCTAssertEqual("Ιωσήφ", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Κωσταντίνος"
+               XCTAssertEqual("Κωσταντίνε", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Αναστάσης"
+               XCTAssertEqual("Αναστάση", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Αυγουστίνος"
+               XCTAssertEqual("Αυγουστίνε", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Πλάτων"
+               XCTAssertEqual("Πλάτων", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Πλάτωνας"
+               XCTAssertEqual("Πλάτωνα", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Παύλος"
+               XCTAssertEqual("Παύλο", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Κωσταντίνα"
+               XCTAssertEqual("Κωσταντίνα", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Μαριλένα"
+               XCTAssertEqual("Μαριλένα", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Δημήτριος"
+               XCTAssertEqual("Δημήτριε", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Δημητρός"
+               XCTAssertEqual("Δημητρό", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Αντωνάκος"
+               XCTAssertEqual("Αντωνάκο", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Σέργιος"
+               XCTAssertEqual("Σέργιε", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Παπαδόπουλος"
+               XCTAssertEqual( "Παπαδόπουλε", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Άννα"
+               XCTAssertEqual("Άννα", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Λαμπρινή"
+               XCTAssertEqual( "Λαμπρινή", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Χρήστος"
+               XCTAssertEqual("Χρήστο", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Ιορδάνης"
+               XCTAssertEqual( "Ιορδάνη", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Ανδρίτσος"
+               XCTAssertEqual("Ανδρίτσο", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Mark"
+               XCTAssertEqual("Mark", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Harry"
+               XCTAssertEqual("Harry", VocativeUtils.getVocativeName(testName!))
+
+               testName = "Μ"
+               XCTAssertEqual("Μ", VocativeUtils.getVocativeName(testName!))
+
+               testName = "4"
+               XCTAssertEqual("4", VocativeUtils.getVocativeName(testName!))
+
+               testName = ""
+               XCTAssertEqual("", VocativeUtils.getVocativeName(testName!))
+
     }
 
     func testPerformanceExample() throws {
